8b96160cbcf995b4214cd7785567ce6f5ce07d60
CarEsteban <car23016@uvg.edu.gt>
2023-11-01 16:34:14 -0600

    antes de la escritura de datos

--- Tienda.java
+++ Tienda.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Tienda{
     public static void main(String[] args) throws IOException {
         int menu, opc;
-        boolean continuar=true;
+        boolean continuar=true,continuarSubMenu=true;
         Scanner scanner = new Scanner(System.in);
         File archivo = new File("dispositivos.csv");
         ArrayList<DispositivoElectronico> dispositivos = new ArrayList<DispositivoElectronico>();
@@ -30,9 +30,84 @@ public class Tienda{
                     break;
                 case 2:
                     System.out.println("VALIDACION DISPOSITIVOS ENCENDIDOS O APAGADOS");
+                    mostrarDispositivosPorEstado(dispositivos, true);
+                    mostrarDispositivosPorEstado(dispositivos, false);
                     break;
                 case 3:
-                    dispositivos.get(0).getEstado();
+                
+                    while(continuarSubMenu){
+                        System.out.println("Desea cambiar el estado de uno existente (1) o agregar uno (2)?");
+                        menu = scanner.nextInt();
+                        scanner.nextLine();
+
+                        switch (menu) {
+                            case 1:
+                                String cambiar;
+                                boolean estadoActual=false;
+                                System.out.println("Ingrese el modelo o marca exacto para cambiar el estado");
+                                imprimirDispositivos(dispositivos, "Telefono");
+                                imprimirDispositivos(dispositivos, "Computadora");
+                                cambiar = scanner.nextLine();
+                                for (DispositivoElectronico dispositivo : dispositivos) {
+                                    if(dispositivo instanceof Telefono && cambiar.equals(((Telefono)dispositivo).getModelo())){
+                                        System.out.println("Estado actual: " + dispositivo.getEstado());
+                                        if(dispositivo.getEstado()){
+                                            dispositivo.apagar();
+                                        }else{
+                                            dispositivo.encender();
+                                        }
+                                        System.out.println("Estado cambiado: " + dispositivo.getEstado());
+
+                                    }else if(dispositivo instanceof Computadora && cambiar.equals(((Computadora)dispositivo).getMarca())){    
+                                        System.out.println("Estado actual: " + dispositivo.getEstado());
+                                        if(dispositivo.getEstado()){
+                                            dispositivo.apagar();
+                                            
+                                            try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+                                                String line;
+                                                br.readLine();
+
+                                                while ((line = br.readLine()) != null) {
+                                                    String[] campos = line.split(",");
+                                                    if (campos[1].equals(cambiar)) {
+                                                        String estadoString = estadoActual ? "Encendido" : "Apagado";
+
+                                                        campos[3] = estadoString;
+                                                    }
+
+
+                                                }
+                                            } catch (IOException e) {
+                                                e.printStackTrace();
+                                            }
+
+
+                                        }else{
+                                            dispositivo.encender();
+                                        }
+                                        System.out.println("Estado cambiado: " + dispositivo.getEstado());
+
+                                    }
+                                }
+
+
+                                continuarSubMenu = volverAlMenu(scanner, " a cambiar algo? ");
+                                break;
+                        
+                            case 2:
+                                
+
+
+                                continuarSubMenu = volverAlMenu(scanner, " a cambiar algo? ");
+                                break;
+                        
+                            default:
+                                System.out.println("Esta opcion no existe");
+                                continuarSubMenu=false;
+                                break;
+                        }
+
+                    }
                     break;
                 case 4:
                     System.out.println("Saliendo del programa...");
@@ -107,6 +182,20 @@ public class Tienda{
             }
         }
     }
+    
+    private static void mostrarDispositivosPorEstado(ArrayList<DispositivoElectronico> dispositivos, boolean estado) {
+        String estadoString = estado ? "Encendidos" : "Apagados";
+        System.out.println("Dispositivos " + estadoString + ":");
+    
+        for (DispositivoElectronico dispositivo : dispositivos) {
+            if (dispositivo instanceof Telefono && estado == ((Telefono) dispositivo).getEstado()) {
+                System.out.println("\t"+dispositivo.getTipo()+" - "+dispositivo);
+            } else if (dispositivo instanceof Computadora && estado == ((Computadora) dispositivo).getEstado()) {
+                System.out.println("\t"+dispositivo.getTipo()+" - "+dispositivo);
+            }
+        }
+    }
+    
 
     private static boolean volverAlMenu(Scanner scanner, String eleccion) {
         System.out.println("¿Desea volver" + eleccion + " (1: Sí, 0: No): ");
